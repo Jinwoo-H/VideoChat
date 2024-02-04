c91fe91862d865dc607553d13e66642e459da2a5
Jinwoo <jinwoo0825@gmail.com>
2024-02-04 17:06:38 -0500

    ui changes

--- main.js
+++ main.js
@@ -35,30 +35,40 @@ const callButton = document.getElementById('callButton');
 const callInput = document.getElementById('callInput');
 const answerButton = document.getElementById('answerButton');
 const remoteVideo = document.getElementById('remoteVideo');
-const hangupButton = document.getElementById('hangupButton');
 
 window.onload = async () => {
-  localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-  remoteStream = new MediaStream();
+  try{
+    localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+    remoteStream = new MediaStream();
+  
+    // Push tracks from local stream to peer connection
+    localStream.getTracks().forEach((track) => {
+      pc.addTrack(track, localStream);
 
-  // Push tracks from local stream to peer connection
-  localStream.getTracks().forEach((track) => {
-    pc.addTrack(track, localStream);
-  });
-
-  // Pull tracks from remote stream, add to video stream
-  pc.ontrack = (event) => {
-    event.streams[0].getTracks().forEach((track) => {
-      remoteStream.addTrack(track);
     });
-  };
 
-  webcamVideo.srcObject = localStream;
-  remoteVideo.srcObject = remoteStream;
+    //Mutes local audio stream
+    localStream.getAudioTracks().forEach(track => {
+      track.enabled = false;
+    });
+  
+    // Pull tracks from remote stream, add to video stream
+    pc.ontrack = (event) => {
+      event.streams[0].getTracks().forEach((track) => {
+        remoteStream.addTrack(track);
+      });
+    };
+  
+    webcamVideo.srcObject = localStream;
+    remoteVideo.srcObject = remoteStream;
+  
+    callButton.disabled = false;
+    answerButton.disabled = false;
+    webcamButton.disabled = true;
+  } catch (error) {
+    console.log("Error using webcam - make sure your webcam is connected")
+  }
 
-  callButton.disabled = false;
-  answerButton.disabled = false;
-  webcamButton.disabled = true;
 };
 
 callButton.onclick = async () => {
@@ -104,7 +114,6 @@ callButton.onclick = async () => {
     });
   });
 
-  hangupButton.disabled = false;
 };
 
 answerButton.onclick = async () => {
